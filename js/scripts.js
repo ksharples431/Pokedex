b51f4fdf11f70c16d1b042ccc8e56771c371a2db
Katie Sharples <KSharples431@gmail.com>
2023-01-24 14:49:34 -0500

    Added poke height display

--- js/scripts.js
+++ js/scripts.js
@@ -15,3 +15,24 @@ let pokemonList = [
     types: ['grass', 'poison']
   }
 ];
+
+for(let i = 0; i < pokemonList.length; i++) {
+  // name and height variables declared
+  let pokeName = pokemonList[i].name;
+  let pokeHeight = pokemonList[i].height;
+
+  //loop through poke heights to display coordinated message
+  if (pokemonList[i].height > 1) {
+    document.write(
+      `${pokeName} (height: ${pokeHeight}) - Wow, that's big! <br>`
+    )
+  } else if(pokemonList[i].height < 0.5) {
+    document.write(
+      `${pokeName} (height: ${pokeHeight}) - Wow, that's tiny! <br>`
+    );
+  } else {
+    document.write(
+      `${pokeName} (height: ${pokeHeight}) - Huh, he's pretty average. <br>`
+    );
+  }
+}
