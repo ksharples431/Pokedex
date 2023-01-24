5b87ec51dcf1672cfa20f821a20dc8492242d72b
Katie Sharples <KSharples431@gmail.com>
2023-01-24 13:21:59 -0500

    Added starter pokelist

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
-alert('Hello world');
-
-let favoriteFood = 'pasta'
-
-document.write(favoriteFood);
+let pokemonList = [
+  {
+    name: 'Dewgong',
+    height: 1.7,
+    types: ['ice', 'water']
+  },
+  {
+    name: 'Weedle',
+    height: 0.3,
+    types: ['bug', 'poison']
+  },
+  {
+    name: 'Oddish',
+    height: 0.5,
+    types: ['grass', 'poison']
+  }
+];
